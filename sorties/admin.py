0eaeab56589cf3328e0dd9193c02c79af1ee0532
SimonBeuvain <91960599+Sywno@users.noreply.github.com>
2024-05-23 13:18:01 +0200

    Delete sorties directory

--- sorties/admin.py
+++
@@ -1,7 +0,0 @@
-# sortie_project/sorties/admin.py
-
-from django.contrib import admin
-from .models import Sortie
-
-# Enregistrer le modèle Sortie dans l'admin
-admin.site.register(Sortie)
--- sorties/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('sorties/', views.liste_sorties, name='liste_sorties'),
-    path('sorties/creer/', views.creer_sortie, name='creer_sortie'),
-]
